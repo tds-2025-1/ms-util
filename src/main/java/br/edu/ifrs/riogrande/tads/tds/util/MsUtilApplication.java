2af66c95c2b660557caa8c2a96e014eed09ebfc4
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2025-03-26 20:10:53 -0300

    Teste

--- src/main/java/br/edu/ifrs/riogrande/tads/tds/util/MsUtilApplication.java
+++ src/main/java/br/edu/ifrs/riogrande/tads/tds/util/MsUtilApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsUtilApplication {
-
+	// teste
 	public static void main(String[] args) {
 		SpringApplication.run(MsUtilApplication.class, args);
 	}
