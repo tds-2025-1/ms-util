43868a01b73e4aa5fe25ba493069a9a6b0023bf7
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2025-03-26 19:34:45 -0300

    Setup

---
+++ src/main/java/br/edu/ifrs/riogrande/tads/tds/ms_util/MsUtilApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.riogrande.tads.tds.ms_util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUtilApplication.class, args);
+	}
+
+}
