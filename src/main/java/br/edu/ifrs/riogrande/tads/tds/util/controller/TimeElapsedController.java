a15e5791f6095a45248e0a503d1366544d99dc8a
Emanuelle Fritzen Machado <manufritzenpro@gmail.com>
2025-05-07 23:30:35 -0300

    feat: cria versao2 do endpoint diferenca horario

--- src/main/java/br/edu/ifrs/riogrande/tads/tds/util/controller/TimeElapsedController.java
+++ src/main/java/br/edu/ifrs/riogrande/tads/tds/util/controller/TimeElapsedController.java
@@ -1,9 +1,12 @@
 package br.edu.ifrs.riogrande.tads.tds.util.controller;
 
+import br.edu.ifrs.riogrande.tads.tds.util.controller.dto.ApiResponse;
+import br.edu.ifrs.riogrande.tads.tds.util.controller.dto.TimeElapsedResponseDTOV2;
 import br.edu.ifrs.riogrande.tads.tds.util.dto.TimeElapsedRequest;
 import br.edu.ifrs.riogrande.tads.tds.util.dto.TimeElapsedResponse;
 import br.edu.ifrs.riogrande.tads.tds.util.service.TimeElapsedCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +30,16 @@ public class TimeElapsedController {
                 request.getStartTime(), request.getEndTime());
         
         return ResponseEntity.ok(new TimeElapsedResponse(elapsedTime));
+    }
+    
+    @PostMapping(
+        value = "v2/elapsed",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<ApiResponse<TimeElapsedResponseDTOV2>> calculateTimeElapsedV2(@RequestBody TimeElapsedRequest request) {
+        String elapsedTime = calculatorService.calculateTimeElapsed(
+                request.getStartTime(), request.getEndTime());
+        
+        return ResponseEntity.ok(new ApiResponse<>(new TimeElapsedResponseDTOV2(elapsedTime)));
     }
 }
