ce36a28d02509ba7ef606da8e7994efd5bd12e1b
lucas.coelho <ludico1959@gmail.com>
2025-07-12 23:08:51 -0300

    feat: Add new endpoint for generating UUIDs values

--- src/main/java/br/edu/ifrs/riogrande/tads/tds/util/controller/UuidController.java
+++ src/main/java/br/edu/ifrs/riogrande/tads/tds/util/controller/UuidController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifrs.riogrande.tads.tds.util.service.UuidService;
+import br.edu.ifrs.riogrande.tads.tds.util.controller.dto.UuidResponseDTO;
+import br.edu.ifrs.riogrande.tads.tds.util.controller.dto.ApiResponse;
+
 @RestController
 public class UuidController {
 
@@ -18,6 +21,7 @@ public class UuidController {
         this.uuidService = UuidService;
     }
 
+    @Deprecated(since = "2025-07-12", forRemoval = false)
     @GetMapping(
         value = "/uuid", 
         produces = MediaType.APPLICATION_JSON_VALUE)
@@ -34,6 +38,22 @@ public class UuidController {
                 .body(List.of(e.getMessage())); 
         }
 
+    }
+
+    @GetMapping(
+        value = "/api/v1/uuid", 
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<UuidResponseDTO>> generateV1(
+        @RequestParam(name = "no_passwords", defaultValue = "1") int noPasswords
+    ) {
+        try {
+            List<String> uuids = this.uuidService.generate(noPasswords);
+            UuidResponseDTO response = new UuidResponseDTO(uuids);
+
+            return ResponseEntity.ok(new ApiResponse<>(response));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
